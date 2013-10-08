39f2ea713b57c8a3d7533e41552fa3c5286ad876
Ray Offiah <ray.offiah@appsbroker.com>
2013-10-08 13:06:07 +0100

    Converted 'not found' stuff

---
+++ src/test/java/controllers/NotFoundTest.kt
@@ -0,0 +1,35 @@
+package controllers
+
+import ninja.NinjaTest
+import org.junit.Test
+import com.google.common.collect.Maps
+import java.util.HashMap
+import org.apache.http.HttpResponse
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+/**
+ * Created by raymond on 08/10/2013.
+ */
+public class NotFoundTest : NinjaTest() {
+
+    [Test]
+    fun testThatNotFoundWorks() {
+
+        // Create headers
+        val headers: HashMap<String, String>? = Maps.newHashMap()
+
+        // Get raw response
+        val httpResponse = ninjaTestBrowser?.makeRequestAndGetResponse("${getServerAddress()}/_non_existing_url", headers)
+
+        // make sure the status code is correct:
+        assertEquals(404, httpResponse?.getStatusLine()?.getStatusCode())
+
+        val content = ninjaTestBrowser?.makeRequest("${getServerAddress()}/_non_existing_url", headers)
+
+        // Check that we get a working "not found" template from views/system/404notFound.ftl.html
+
+        assertTrue(content?.contains("Oops. Not found.")!!)
+
+    }
+}
