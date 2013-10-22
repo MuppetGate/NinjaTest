fe85608e99539f83e3d2da06da942030b9e79482
Muppet Gate <muppet.gate@yahoo.com>
2013-10-22 10:57:45 +0100

    Converted integration tests

---
+++ src/test/java/example/ExampleApiTest.kt
@@ -0,0 +1,20 @@
+package example
+
+import ninja.NinjaTest
+import org.junit.Test
+import kotlin.test.assertTrue
+
+/**
+ * Created by raymond on 22/10/2013.
+ */
+public class ExampleApiTest: NinjaTest() {
+
+    [Test]
+    fun testThatStaticAssetsWork() {
+
+        val apiCallResult = ninjaTestBrowser?.makeJsonRequest("${getServerAddress()}/person")
+        println("apicallresult: " + apiCallResult)
+        assertTrue((apiCallResult?.startsWith("{\"name\":\"zeeess name -"))!!)
+
+    }
+}
---
+++ src/test/java/example/ExampleIntegrationTest.kt
@@ -0,0 +1,44 @@
+package example
+
+import ninja.NinjaFluentLeniumTest
+import kotlin.test.assertTrue
+import org.junit.Test
+
+/**
+ * Created by raymond on 22/10/2013.
+ */
+public class ExampleIntegrationTest: NinjaFluentLeniumTest() {
+
+    [Test]
+    fun testThatStaticAssetsWork(): Unit {
+
+        goTo("${getServerAddress()}/assets/css/bootstrap.css")
+        assertTrue((pageSource()?.contains("Bootstrap"))!!)
+
+    }
+
+    [Test]
+    fun testThatInvalidStaticAssetsAreNotFound(): Unit {
+
+        goTo("${getServerAddress()}/assets/css/INVALID_FILE")
+        assertTrue((pageSource()?.isEmpty())!!)
+
+    }
+
+    [Test]
+    fun testIndexRoute(): Unit {
+
+        goTo(getServerAddress())
+        assertTrue((pageSource()?.contains("Ninja web framework"))!!)
+
+    }
+
+    [Test]
+    fun testExamples(): Unit {
+
+        goTo("${getServerAddress()}/examples")
+        assertTrue((pageSource()?.contains("Examples"))!!)
+
+
+    }
+}
