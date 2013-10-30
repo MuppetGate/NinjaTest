b60e33ac212be3088d142175d75a534c8f90f705
Muppet Gate <muppet.gate@yahoo.com>
2013-10-30 10:24:17 +0000

    Got the Async stuff going

--- src/main/java/conf/Routes.kt
+++ src/main/java/conf/Routes.kt
@@ -14,6 +14,7 @@ import ninja.AssetsController
 import ninja.utils.NinjaProperties
 import com.google.inject.Inject
 import controllers.ApplicationController
+import controllers.AsyncController
 
 /**
  * Created by raymond on 08/10/2013.
---
+++ src/main/java/controllers/AsyncController.kt
@@ -0,0 +1,39 @@
+package controllers
+
+import com.google.inject.Singleton
+import java.util.concurrent.Executors
+import ninja.Context
+import ninja.Result
+import ninja.Results
+import java.util.concurrent.TimeUnit
+import ninja.lifecycle.Dispose
+import java.lang.Long.parseLong
+
+/**
+ * Created by raymond on 28/10/2013.
+ */
+[Singleton]
+public class AsyncController {
+
+    private  val executorService = Executors.newSingleThreadScheduledExecutor()
+
+    fun asyncEcho(ctx: Context) : Result? {
+        executorService.schedule( {
+
+
+                ctx.returnResultAsync(Results.json()?.render(ctx.getParameter("message")))
+
+
+
+        }, ctx.getParameter("timeout")!!.toLong(), TimeUnit.MILLISECONDS)
+
+        return Results.async()
+    }
+
+    [Dispose]
+    fun shutDownExecutor(): Unit {
+
+        executorService.shutdown()
+
+    }
+}
