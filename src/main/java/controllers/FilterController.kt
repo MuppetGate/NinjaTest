24ef871f59a8062202e0d92694f29ad7a0d6f304
Muppet Gate <muppet.gate@yahoo.com>
2013-11-01 09:04:22 +0000

    Bit of trouble with the FilterController caused by instability in the Kotlin plugin.

---
+++ src/main/java/controllers/FilterController.kt
@@ -0,0 +1,37 @@
+package controllers
+
+import com.google.inject.Singleton
+import ninja.Result
+import ninja.Context
+import ninja.FilterWith
+import ninja.SecureFilter
+
+import ninja.Results
+import ninja.Cookie
+import filters.LoggerFilter
+import filters.TeaPotFilter
+
+/**
+ * Created by raymond on 01/11/2013.
+ */
+[Singleton]
+public class FilterController {
+
+    [FilterWith(javaClass<SecureFilter>())]
+    fun filter(context: Context): Result? {
+
+        context.getCookies()!!.forEach {
+
+            println("cookie: ${it.getName()}" )
+        }
+
+        return Results.html()!!.addCookie(Cookie.builder("myname", "myvalue")!!.build())
+    }
+
+    [FilterWith(javaClass<LoggerFilter>(), javaClass<TeaPotFilter>())]
+    fun teapot(context: Context): Result? {
+
+        return Results.html()
+    }
+
+}
