9d210c71f2ef6c4a3cc2e8c12d98fb00557d6e29
Muppet Gate <muppet.gate@yahoo.com>
2013-11-04 16:33:27 +0000

    RoutesTest converted

---
+++ src/test/java/conf/RoutesTest.kt
@@ -0,0 +1,29 @@
+package conf
+
+import ninja.NinjaRouterTest
+import org.junit.Test
+import controllers.ApplicationController
+
+/**
+ * Created by raymond on 04/11/2013.
+ */
+public class RoutesTest : NinjaRouterTest() {
+
+    [Test]
+    fun testRoutesAreHiddenFromProduction(): Unit {
+
+        startServerInProdMode()
+
+        // Make sure you can't see the routing stuff in production
+        aRequestLike("GET", "/_test/testPage")?.isNotHandledByRoutesInRouter()
+    }
+
+    [Test]
+    fun testRoutes(): Unit {
+
+        startServer()
+
+        aRequestLike("GET", "/")?.isHandledBy(javaClass<ApplicationController>(), "index")
+        aRequestLike("GET", "/examples")?.isHandledBy(javaClass<ApplicationController>(), "examples")
+    }
+}
